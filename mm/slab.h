06e2d7b72e28fe7a77d2dc25cf577b2be8ef22f8
Daniel Micay <danielmicay@gmail.com>
2017-05-03 11:50:53 -0400

    bug on kmem_cache_free with the wrong cache
    At least when CONFIG_BUG_ON_DATA_CORRUPTION is enabled.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>

--- mm/slab.h
+++ mm/slab.h
@@ -453,10 +453,15 @@ static inline struct kmem_cache *cache_from_obj(struct kmem_cache *s, void *x)
 		return s;
 
 	cachep = virt_to_cache(x);
-	if (WARN(cachep && cachep != s,
-		  "%s: Wrong slab cache. %s but object is from %s\n",
-		  __func__, s->name, cachep->name))
+	if (cachep && cachep != s) {
+#ifdef CONFIG_BUG_ONE_DATE_CORRUPTION
+		BUG();
+#else
+		WARN_ONCE(1, "%s: Wrong slab cache. %s but object is from %s\n",
+			     __func__, s->name, cachep->name);
+#endif
 		print_tracking(cachep, x);
+	}
 	return cachep;
 }
 
