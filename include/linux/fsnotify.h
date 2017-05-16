b070ca7e25967a4cac4f642c37a5c952ee0055e1
Daniel Micay <danielmicay@gmail.com>
2017-05-16 18:26:10 -0400

    restrict device timing side channels
    Based on the public grsecurity patches.

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -193,6 +193,9 @@ static inline void fsnotify_access(struct file *file)
 	struct inode *inode = file_inode(file);
 	__u32 mask = FS_ACCESS;
 
+	if (is_sidechannel_device(inode))
+		return;
+
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
 
@@ -208,6 +211,9 @@ static inline void fsnotify_modify(struct file *file)
 	const struct path *path = &file->f_path;
 	struct inode *inode = file_inode(file);
 	__u32 mask = FS_MODIFY;
+
+	if (is_sidechannel_device(inode))
+		return;
 
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
