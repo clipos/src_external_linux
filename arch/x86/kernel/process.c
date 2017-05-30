fef906117545c0602b23190551ba579415dfb8f3
Daniel Micay <danielmicay@gmail.com>
2017-05-30 07:19:48 -0400

    x86_64: match arm64 brk randomization entropy
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -43,6 +43,8 @@
 #include <asm/io_bitmap.h>
 #include <asm/proto.h>
 #include <asm/frame.h>
+#include <asm/elf.h>
+#include <linux/sizes.h>
 
 #include "process.h"
 
@@ -904,7 +906,10 @@ unsigned long arch_align_stack(unsigned long sp)
 
 unsigned long arch_randomize_brk(struct mm_struct *mm)
 {
-	return randomize_page(mm->brk, 0x02000000);
+	if (mmap_is_ia32())
+		return randomize_page(mm->brk, SZ_32M);
+	else
+		return randomize_page(mm->brk, SZ_1G);
 }
 
 /*
