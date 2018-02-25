831d2a803379ff677afe2a8ea0aab25f09ba35a0
Daniel Micay <danielmicay@gmail.com>
2018-02-25 03:26:45 -0500

    hard-wire legacy checkreqprot option to 0
    The userspace API is left intact for compatibility.
    
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -135,21 +135,7 @@ static int __init selinux_enabled_setup(char *str)
 __setup("selinux=", selinux_enabled_setup);
 #endif
 
-static unsigned int selinux_checkreqprot_boot =
-	CONFIG_SECURITY_SELINUX_CHECKREQPROT_VALUE;
-
-static int __init checkreqprot_setup(char *str)
-{
-	unsigned long checkreqprot;
-
-	if (!kstrtoul(str, 0, &checkreqprot)) {
-		selinux_checkreqprot_boot = checkreqprot ? 1 : 0;
-		if (checkreqprot)
-			pr_warn("SELinux: checkreqprot set to 1 via kernel parameter.  This is deprecated and will be rejected in a future kernel release.\n");
-	}
-	return 1;
-}
-__setup("checkreqprot=", checkreqprot_setup);
+static const unsigned int selinux_checkreqprot_boot;
 
 /**
  * selinux_secmark_enabled - Check to see if SECMARK is currently enabled
