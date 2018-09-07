2b1dcd85f7e941f6709cc110129b39ae228f33c1
Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
2018-09-07 10:05:57 +0200

    Revert "Partially revert "block: fail op_is_write() requests to read-only partitions""
    This reverts commit c9c16a87da687ee730032d564764da9b506fcf90.
    
    We should not suffer from the issue this partial revert aims at solving,
    so let's not weaken the "never write to read-only partitions" in our
    kernel.
    
    Change-Id: Ia9d600fb472c8ff4b5e46484a086c3b4e8b9b6b0
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>

--- block/blk-core.c
+++ block/blk-core.c
@@ -2174,12 +2174,11 @@ static inline bool bio_check_ro(struct bio *bio, struct hd_struct *part)
 	if (part->policy && op_is_write(bio_op(bio))) {
 		char b[BDEVNAME_SIZE];
 
-		WARN_ONCE(1,
+		printk(KERN_ERR
 		       "generic_make_request: Trying to write "
 			"to read-only block-device %s (partno %d)\n",
 			bio_devname(bio, b), part->partno);
-		/* Older lvm-tools actually trigger this */
-		return false;
+		return true;
 	}
 
 	return false;
