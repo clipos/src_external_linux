17db8f39cd610100a52a6cdbe3eb6fbab5674beb
Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
2019-03-26 15:22:05 +0100

    Revert "netfilter: nf_tables: fix set double-free in abort path"
    This reverts commit c55cfcf577f1520254dc8cc7f3be832d32304330.
    
    It has nothing to do with linux-hardened and should be backported to
    stable or temporarily by ourselves.
    
    Change-Id: I3f2361745510d1dfd52ec47193cb905faf01e7d9
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -416,8 +416,7 @@ struct nft_set {
 	unsigned char			*udata;
 	/* runtime data below here */
 	const struct nft_set_ops	*ops ____cacheline_aligned;
-	u16				flags:13,
-					bound:1,
+	u16				flags:14,
 					genmask:2;
 	u8				klen;
 	u8				dlen;
@@ -1330,12 +1329,15 @@ struct nft_trans_rule {
 struct nft_trans_set {
 	struct nft_set			*set;
 	u32				set_id;
+	bool				bound;
 };
 
 #define nft_trans_set(trans)	\
 	(((struct nft_trans_set *)trans->data)->set)
 #define nft_trans_set_id(trans)	\
 	(((struct nft_trans_set *)trans->data)->set_id)
+#define nft_trans_set_bound(trans)	\
+	(((struct nft_trans_set *)trans->data)->bound)
 
 struct nft_trans_chain {
 	bool				update;
